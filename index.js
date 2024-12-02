a7c32c4f61a12988f0f611f0a60e6727430d8431
beatrice <wairimubeatrice209@gmail.com>
2024-12-02 14:02:53 +0300

    HELLO

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+//Random
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+// mod
+const num3 = 14;
+const num4 = 5;  
+const mod = num3 % num4;
+console.log(mod);
+// max
+const max = Math.max(7, 8, 17, 20, 0);
+console.log(max);
